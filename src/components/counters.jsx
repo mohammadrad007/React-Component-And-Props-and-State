ae49b9f2247ae2fe41605e94b3157aa1e4f6a65f
mohammad mohammadi <saberzx313@gmail.com>
2019-03-01 19:39:57 +0330

    props for App

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,13 +2,27 @@ import React, { Component } from 'react'
 import Counter from './counter'; 
 
 class Counters extends Component {
-    state = {  }
+    state = { 
+        counters : [
+            {
+                id :1, value:0
+            },
+            {
+                id :2, value:5
+            },
+            {
+                id :3, value:2
+            },
+            {
+                id :4, value:7
+            }
+        ]
+    };
     render() { 
         return ( 
             <>
-                <Counter />
-                <Counter />
-                <Counter />
+               {this.state.counters.map( counter =>
+                 <Counter key={counter.id} value={counter.value} /> )}
             </>
         );
     }
